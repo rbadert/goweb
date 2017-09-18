a90aa4d1553c5a366589262255ab9956810c6929
Reto Badertscher <retobadertscher@gmail.com>
2017-09-18 18:29:04 +0200

    Initialization

--- main.go
+++ main.go
@@ -13,12 +13,32 @@ func main() {
 	// that can recover from any http-relative panics
 	// and log the requests to the terminal.
 	app.Use(recover.New())
-	app.Use(logger.New())
+	//app.Use(logger.New())
+	customLogger := logger.New(logger.Config{
+		// Status displays status code
+		Status: true,
+		// IP displays request's remote address
+		IP: true,
+		// Method displays the http method
+		Method: true,
+		// Path displays the request path
+		Path: true,
+		//Columns: true,
+		// if !empty then its contents derives from `ctx.Values().Get("logger_message")
+		// will be added to the logs.
+		MessageContextKey: "logger_message",
+	})
+
+	app.Use(customLogger)
+	// load templaes
+	app.RegisterView(iris.HTML("./app/views", ".html").Reload(true))
 
 	// Method:   GET
 	// Resource: http://localhost:8080/
 	app.Handle("GET", "/", func(ctx context.Context) {
-		ctx.HTML("<b>Welcome!</b>")
+		ctx.ViewData("message", "My Message")
+		//ctx.HTML("<b>Welcome!</b>")
+		ctx.View("hello.html")
 	})
 
 	// same as app.Handle("GET", "/ping", [...])
@@ -32,6 +52,12 @@ func main() {
 	// Resource: http://localhost:8080/hello
 	app.Get("/hello", func(ctx context.Context) {
 		ctx.JSON(context.Map{"message": "Hello iris web framework."})
+	})
+	// or catch all http errors:
+	app.OnAnyErrorCode(customLogger, func(ctx context.Context) {
+		// this should be added to the logs, at the end because of the `logger.Config#MessageContextKey`
+		ctx.Values().Set("logger_message","a dynamic message passed to the logs")
+		ctx.Writef("My Custom error page")
 	})
 
 	// http://localhost:8080
